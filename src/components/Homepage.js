b5bd88e722cb3a8cbc17d7eec4c92852485fc3af
ltreven <ltreven@gmail.com>
2020-02-19 02:45:40 +0100

    pre load database feature

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,8 +1,76 @@
 import React, { Component } from 'react';
+import { Button } from 'reactstrap';
 import Carrusel from './Carrusel';
 import '../Home.css';
+import { baseUrl } from '../BaseUrl.js';
+import { MOVIES }  from '../loaddb.js';
 
 class HomePage extends Component {
+
+    constructor(props) {
+        super(props);
+        this.loadDB = this.loadDB.bind(this); 
+        this.loginLoadDB = this.loginLoadDB.bind(this); 
+    }
+
+    loginLoadDB() {
+        console.log("loggin in with load db user");
+        // LOGIN
+        fetch(baseUrl + 'users/login', {
+            headers: {'Content-Type': 'application/json'},
+            method: "POST",
+            body: JSON.stringify(
+                { username: "loaduser", password: "1234"})
+        })
+        .then(res => res.json())
+        .then((data) => {
+            if (data.success) {
+                // for each movie on MOVIES:
+                // CREATE MOVIE
+                MOVIES.forEach((movie, index) => {
+                    fetch(baseUrl + 'movies/', {
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'Authorization': 'Bearer ' + data.token
+                        },
+                        method: "POST",
+                        body: JSON.stringify(movie)
+                    })
+                    .catch((err) => console.log(err));
+                });
+                // refresh
+                alert("movies loaded successfully. Please frefresh.");
+                
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+
+    }
+
+    loadDB() {
+        //signup on an empty database
+        console.log("Signing up with load db user");
+        fetch(baseUrl + 'users/signup', {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            method: "POST",
+            body: JSON.stringify(
+                { fullName: "loaduser", username: "loaduser", password: "1234" })
+        })
+        .then(res => res.json())
+        .then((data) => {
+            this.loginLoadDB();
+        })
+        .catch((err) => {
+            console.log(err);
+            // try to login with 
+            this.loginLoadDB();
+        });
+        
+    }
 
     render () {
         return (
@@ -10,17 +78,27 @@ class HomePage extends Component {
                 <div className="row rosa">
                     <div className="side">
                         <div>
-                        <h4>
-                            Search, see and publish your own movies.
-                            You don't have to be authenticated to navigate, but 
-                            to create, update or delete movies, you must
-                            login. If you don't have an account, create one using
-                            the signup feature.
-                        </h4>
-                        <br/><br/>
-                        <div className="homebutton darkblue">
-                            <a href="/list">SEE MOVIES</a>
-                        </div>
+                            <h4>
+                                Search, see and publish your own movies.
+                                You don't have to be authenticated to navigate, but 
+                                to create, update or delete movies, you must
+                                login. If you don't have an account, create one using
+                                the signup feature.
+                            </h4>
+                            <br/>
+                            <div className="homebutton darkblue">
+                                <a href="/list">SEE MOVIES</a>
+                            </div>
+                            <br/>
+                            <br/>
+                            <h5>For a full test experience, click the button below.
+                                It will full your database with some nice movies.
+                            </h5>
+                            <div>
+                                <Button color="danger" className="menu" onClick={this.loadDB}>
+                                    LOAD DB
+                                </Button>
+                            </div>
                         </div>
                     </div>
                     <div className="main darjeeling">
@@ -35,17 +113,19 @@ class HomePage extends Component {
                         <div>
                         <h1>TECH STACK</h1>
                         <h4>Database</h4>
-                        <h4>The platform's database is MongoDB.</h4>
+                        <h5>
+                            The platform's database is MongoDB. 
+                        </h5>
                         <br/>
                         <h4>Back End</h4>
-                        <h4>The backend was developed in Javascript using Node
+                        <h5>The backend was developed in Javascript using Node
                             and Express as the web server.
-                            It exposes a secure and restful API.</h4>
+                            It exposes a secure and restful API.</h5>
                         <br/>
                         <h4>Front End</h4>
-                        <h4>Developed using React. Responsiveness was achieved
+                        <h5>Developed using React. Responsiveness was achieved
                             by the use of Flexbox and CSS Grid.
-                        </h4>
+                        </h5>
                         <br/><br/>
                         <div className="homebutton rosa">
                             <a href="https://documenter.getpostman.com/view/2306941/SzKPUfw5?version=latest" target="_blank" rel="noopener noreferrer">
---
+++ src/loaddb.js
@@ -0,0 +1,103 @@
+export const MOVIES =
+    [
+        {
+            title: "Django Unchained",
+            posterImageUrl: "http://localhost:3000/img/django.png",
+            director: "Quentin Tarantino",
+            release: "2013-01-18T10:00:00", 
+            category: "comedy",
+            description: "No sul dos Estados Unidos, anos antes da Guerra Civil, um ex-escravo Django faz uma aliança inesperada com o caçador de recompensas Schultz para caçar os criminosos mais procurados do país e resgatar sua esposa de um fazendeiro que força seus escravos a participarem de competições mortais.",
+            score: 10
+        },
+        {
+            title: "Two Catalonias",
+            posterImageUrl: "http://localhost:3000/img/2catalonias.jpg",
+            director: "Gerardo Olivares, Alvaro Longoria",
+            release: "2018-09-28T10:00:00", 
+            category: "documentary",
+            description: "Two Catalonias is a 2018 documentary film directed by Álvaro Longoria and Gerardo Olivares, about the ideological differences surrounding the election in Spain. The film was released by Netflix on September 28, 2018",
+            score: 10
+        },
+        {
+            title: "God Father",
+            posterImageUrl: "http://localhost:3000/img/godfather.jpg",
+            director: "Francis Ford Coppola",
+            release: "1972-10-20T10:00:00", 
+            category: "drama",
+            description: "The Godfather is a 1972 American crime film directed by Francis Ford Coppola and produced by Albert S. Ruddy, based on Mario Puzo's best-selling novel of the same name.",
+            score: 10
+        },
+        {
+            title: "Inglorious Basterds",
+            posterImageUrl: "http://localhost:3000/img/inglorious.jpg",
+            director: "Francis Ford Coppola",
+            release: "2009-05-20T10:00:00", 
+            category: "comedy",
+            description: "Inglourious Basterds is a 2009 alternate history black comedy war film written and directed by Quentin Tarantino and starring Brad Pitt, Christoph Waltz, Michael Fassbender, Eli Roth, Diane Kruger, Daniel Brühl, Til Schweiger and Mélanie Laurent.",
+            score: 10
+        },
+        {
+            title: "Kill Bill",
+            posterImageUrl: "http://localhost:3000/img/killbill2.jpg",
+            director: "Quentin Tarantino",
+            release: "2003-10-10T10:00:00", 
+            category: "comedy",
+            description: "A woman in a wedding dress, the Bride, lies wounded in a chapel in El Paso, Texas, having been attacked by the Deadly Viper Assassination Squad. She tells their leader, Bill, that she is pregnant with his baby. He shoots her in the head.",
+            score: 10
+        },
+        {
+            title: "Pulp Fiction",
+            posterImageUrl: "http://localhost:3000/img/pulp.jpg",
+            director: "Quentin Tarantino",
+            release: "1994-05-21T10:00:00", 
+            category: "comedy",
+            description: "Pulp Fiction is a 1994 American crime film written and directed by Quentin Tarantino, who conceived it with Roger Avary.",
+            score: 10
+        },
+        {
+            title: "Green Book",
+            posterImageUrl: "http://localhost:3000/img/green.jpg",
+            director: "Peter Farrelly",
+            release: "2018-09-11T10:00:00", 
+            category: "drama",
+            description: "In 1962, New York City bouncer Frank Tony Lip Vallelonga (Viggo Mortensen) is working at the Copacabana Night Club. The club is then closed for renovations, leaving Tony without a job.",
+            score: 10
+        },
+        {
+            title: "Jojo Rabbit",
+            posterImageUrl: "http://localhost:3000/img/jojo.jpg",
+            director: "Taika Waititi",
+            release: "2019-10-18T10:00:00", 
+            category: "comedy",
+            description: "Jojo Rabbit is a 2019 American comedy-drama film written and directed by Taika Waititi, based on Christine Leunens's book Caging Skies.",
+            score: 10
+        },
+        {
+            title: "Neverending Story",
+            posterImageUrl: "http://localhost:3000/img/neverending.jpg",
+            director: "Wolfgang Petersen",
+            release: "1984-04-06T10:00:00", 
+            category: "drama",
+            description: "Jhe NeverEnding Story (German: Die Unendliche Geschichte) is a 1984 English-language German fantasy film directed and co-written by Wolfgang Petersen (in his first English-language film), and based on the novel The Neverending Story by Michael Ende",
+            score: 10
+        },
+        {
+            title: "The Birds",
+            posterImageUrl: "http://localhost:3000/img/hitchcock.jpg",
+            director: "Alfred Hitchcock",
+            release: "1963-03-28T10:00:00", 
+            category: "documentary",
+            description: "The Birds is a 1963 American horror-thriller film directed and produced by Alfred Hitchcock. Loosely based on the 1952 story of the same name by Daphne du Maurier, it focuses on a series of sudden and unexplained violent bird attacks on the people of Bodega Bay, California, over the course of a few days.",
+            score: 10
+        },
+        {
+            title: "Parasite",
+            posterImageUrl: "http://localhost:3000/img/parasite.jpg",
+            director: "Bong Joon-ho",
+            release: "2019-05-21T10:00:00", 
+            category: "comedy",
+            description: "Parasite (Korean: 기생충; RR: Gisaengchung) is a 2019 South Korean tragicomedy thriller film directed by Bong Joon-ho, who also co-wrote the screenplay with Han Jin-won",
+            score: 10
+        }
+    ];
+    
